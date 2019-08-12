9f6ee9dba5569dc9af5a6ae0a9e7965cbbb3cd55
吕宗国 <1741509527@qq.com>
2019-08-12 15:24:15 +0800

    Update Draw.php

--- application/index/controller/Draw.php
+++ application/index/controller/Draw.php
@@ -11,8 +11,8 @@ class Draw extends Common
     {
     	// dump();
         // return '1';
-    	$id = $request->get('id');
-    	$data = Db::table('draw')->where('id',$id)->find();
+    	$id = $request->get('id') ?? '';
+    	$data = Db::table('draw')->where('id',$id)->order('id desc')->find();
 
         return $this->fetch('index',['data'=>$data]);
         //加载抽奖页面
